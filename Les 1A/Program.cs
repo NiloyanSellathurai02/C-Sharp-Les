3edb949c287f3323447ab53c9fce8fc9d7acb714
NiloyanSellathurai02 <n.sella2001@gmail.com>
2024-02-06 11:23:58 +0100

    first commit

---
+++ Les 1A/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void divideNumber(int a , int b ){
+if(b == 0){
+    Console.WriteLine("Undefined");
+}else if (Math.Abs(b) < 1){
+    Console.WriteLine("Small Divider:" + (a / b));
+}else{
+    int result = a /b ;
+    string resultasString = result.ToString();
+    Console.WriteLine("Result:" +resultasString);
+}
+} 
+
+string globalNumber = "5";
+
+
+
+divideNumber(10 , 5);
